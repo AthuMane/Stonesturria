e9ec14c5532933b949392ae6173140c0e29366d6
AthuMane <162869602+AthuMane@users.noreply.github.com>
2024-03-12 14:57:13 +0530

    Updated

--- Stonesturria/Assets/Scripts/MainMenu.cs
+++ Stonesturria/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    bool paused = false;
+    public GameObject pauseMenu;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused) 
+            {
+                Time.timeScale = 0;
+                paused = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                paused = false;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        paused = false;
+        pauseMenu.SetActive(false);
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
